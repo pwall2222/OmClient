628cdcb1031741307c32fda62ac1dd86ee7f1563
PWall <34860495+PWall2222@users.noreply.github.com>
2021-02-24 22:02:59 +0100

    Added types to a separate file

---
+++ TS/types.ts
@@ -0,0 +1,49 @@
+interface backendEvent {
+	name: string;
+	data?: any;
+}
+
+interface domObject {
+	tag: string;
+	args?: args;
+	child?: domObject;
+}
+
+interface command {
+	name: string;
+	description: string;
+	exec: Function;
+}
+
+interface args {
+	accessKey?: string;
+	autocapitalize?: string;
+	dir?: string;
+	draggable?: boolean;
+	hidden?: boolean;
+	innerText?: string;
+	lang?: string;
+	spellcheck?: boolean;
+	title?: string;
+	translate?: boolean;
+	autofocus?: boolean;
+	nonce?: string;
+	tabIndex?: number;
+	contentEditable?: string;
+	enterKeyHint?: string;
+	inputMode?: string;
+	className?: string;
+	id?: string;
+	innerHTML?: string;
+	outerHTML?: string;
+	scrollLeft?: number;
+	scrollTop?: number;
+	slot?: string;
+	nodeValue?: string | null;
+	textContent?: string | null;
+	/* [key: string]: unknown */
+}
+
+type dStatus = "stop" | "rlly" | "new";
+type author = "you" | "stranger";
+type pcOption = "Offer" | "Answer";
