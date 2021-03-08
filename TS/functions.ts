9793369a47914f74f4716fdf66a0286db88a06d3
PWall <34860495+PWall2222@users.noreply.github.com>
2021-03-08 18:40:26 +0100

    Moved generic functions to another file

---
+++ TS/functions.ts
@@ -0,0 +1,40 @@
+const clearArray = function (array: any[]) {
+	return array.splice(0, array.length);
+};
+
+const createElement = function (domObject: domObject) {
+	const element = document.createElement(domObject.tag);
+	if (domObject.args) {
+		for (const key in domObject.args) {
+			element[key] = domObject.args[key];
+		}
+	}
+	if (domObject.child) {
+		element.appendChild(createElement(domObject.child));
+	}
+	return element;
+};
+
+const createChild = function (parent: string, domObject: domObject) {
+	const child = createElement(domObject);
+	document.querySelector(parent).appendChild(child);
+};
+
+const createChildBefore = function (parent: string, reference: string, domObject: domObject) {
+	const child = createElement(domObject);
+	const parentNode = document.querySelector(parent);
+	const referenceNode = parentNode.querySelector(reference);
+	parentNode.insertBefore(child, referenceNode);
+};
+
+const clearChilds = function (nodeName: string) {
+	const node = document.querySelector(nodeName);
+	node.textContent = "";
+};
+
+const clearAllElements = function (nodeName: string) {
+	const nodes = document.querySelectorAll(nodeName);
+	nodes.forEach((element) => {
+		element.remove();
+	});
+};
--- TS/index.ts
+++ TS/index.ts
@@ -15,47 +15,6 @@ const WEB_RTC_MEDIA_CONSTRAINTS = {
 	},
 };
 
-const clearArray = function (array: any[]) {
-	return array.splice(0, array.length);
-};
-
-const createElement = function (domObject: domObject) {
-	const element = document.createElement(domObject.tag);
-	if (domObject.args) {
-		for (const key in domObject.args) {
-			element[key] = domObject.args[key];
-		}
-	}
-	if (domObject.child) {
-		element.appendChild(createElement(domObject.child));
-	}
-	return element;
-};
-
-const createChild = function (parent: string, domObject: domObject) {
-	const child = createElement(domObject);
-	document.querySelector(parent).appendChild(child);
-};
-
-const createChildBefore = function (parent: string, reference: string, domObject: domObject) {
-	const child = createElement(domObject);
-	const parentNode = document.querySelector(parent);
-	const referenceNode = parentNode.querySelector(reference);
-	parentNode.insertBefore(child, referenceNode);
-};
-
-const clearChilds = function (nodeName: string) {
-	const node = document.querySelector(nodeName);
-	node.textContent = "";
-};
-
-const clearAllElements = function (nodeName: string) {
-	const nodes = document.querySelectorAll(nodeName);
-	nodes.forEach((element) => {
-		element.remove();
-	});
-};
-
 const encodeObject = function (data: object) {
 	const formData: string[] = [];
 	const append = function (key: string, value: string) {
