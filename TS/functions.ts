b0627325d68a620028cfa74d62b009f2bc3fd5db
PWall <34860495+PWall2222@users.noreply.github.com>
2021-03-10 19:53:20 +0100

    Added hash function

--- TS/functions.ts
+++ TS/functions.ts
@@ -45,3 +45,14 @@ const setFirstByIndex = function (array: object[], index: number) {
 		array.unshift(identy[0]);
 	}
 };
+
+const hash = function (string: string) {
+	let hash = 0;
+	if (string.length === 0) return hash;
+	for (let i = 0; i < string.length; i++) {
+		const chr = string.charCodeAt(i);
+		hash = (hash << 5) - hash + chr;
+		hash |= 0;
+	}
+	return hash;
+};
