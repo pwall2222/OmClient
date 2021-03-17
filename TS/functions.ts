0ded7edbf376dfb2864ae38f19cd9318b5fd8fa0
PWall <34860495+PWall2222@users.noreply.github.com>
2021-03-17 20:33:23 +0100

    Refactored hash function

--- TS/functions.ts
+++ TS/functions.ts
@@ -48,11 +48,12 @@ const setFirstByIndex = (array: object[], index: number) => {
 
 const hash = (string: string) => {
 	let hash = 0;
-	if (string.length === 0) return hash;
-	for (let i = 0; i < string.length; i++) {
-		const chr = string.charCodeAt(i);
-		hash = (hash << 5) - hash + chr;
-		hash |= 0;
+	if (string.length !== 0) {
+		for (let i = 0; i < string.length; i++) {
+			const chr = string.charCodeAt(i);
+			hash = (hash << 5) - hash + chr;
+			hash |= 0;
+		}
 	}
 	return hash;
 };
