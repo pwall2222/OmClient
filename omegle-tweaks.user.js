03ebc4c2f6b52213de17d9aaf9a7ed1b3bf59159
PWall <34860495+PWall2222@users.noreply.github.com>
2021-03-19 13:08:14 +0100

    Added page sustitution

--- omegle-tweaks.user.js
+++ omegle-tweaks.user.js
@@ -1,15 +1,22 @@
-// ==UserScript==
-// @name         Omegle-Tweaks
-// @version      1.0
-// @author       PWall
-// @include      https://omegle.com/*
-// @include      https://www.omegle.com/*
-// @run-at       document-end
-// @grant        none
-// ==/UserScript==
+/*
+==UserScript==
+@name         Omegle-Tweaks
+@version      1.0
+@author       PWall
+@include      https://omegle.com/*
+@include      https://www.omegle.com/*
+@run-at       document-end
+@grant        none
+==/UserScript== 
+*/
 
-(function() {
-	document.head.textContent = "";
-	let doc = await fetch("").then(response => response.text());
-	document.write(doc);
+(async function () {
+	document.write("<head><title>Omegle</title></head><body></body>");
+	window.stop()
+	const doc = await fetch("").then(response => response.text());
+	const item = document.createElement("iframe");
+	item.style = "position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none;"
+	document.body.appendChild(item);
+	item.contentDocument.write(doc);
+	item.focus();
 })();
