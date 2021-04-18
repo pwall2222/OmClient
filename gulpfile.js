c5585b0b88d29a593708e5d450bb04a880ffa997
PWall <34860495+PWall2222@users.noreply.github.com>
2021-04-18 22:58:02 +0200

    Added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const { src, task, watch, symlink } = require("gulp");
+const browserSync = require("browser-sync").create();
+const tsc = require("node-typescript-compiler");
+
+const logTask = (message, task, colorNum) => {
+	const white = "\x1b[0m";
+	const color = `\x1b[${colorNum}m`;
+	console.log(`[${color}${task}${white}]${message}`);
+};
+
+const compile = () => tsc.compile({ outDir: "server" });
+
+const compileWatch = () => watch("src/ts/*").on("change", compileLog);
+
+const compileLog = async () => {
+	logTask("Compiling files", "TypeScript", "36");
+	await compile();
+	logTask("Compiling finished", "TypeScript", "36");
+};
+
+const serveFiles = () => {
+	browserSync.init({
+		server: {
+			baseDir: "./server/",
+		},
+		ghostMode: false,
+	});
+
+	watch("server/*").on("change", browserSync.reload);
+};
+
+const compileSync = () => {
+	compileWatch();
+	serveFiles();
+};
+
+const init = () => src("src/page/*").pipe(symlink("server/"));
+
+task("serve", serveFiles);
+
+task("compile", compile);
+
+task("compile-watch", compileWatch);
+
+task("init", init);
+
+task("default", compileSync);
