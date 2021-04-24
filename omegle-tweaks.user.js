a5dafd576ae3859a34129b6ad09fb45c4e9744a1
PWall <34860495+PWall2222@users.noreply.github.com>
2021-04-24 19:39:17 +0200

    Fixed userscript

--- omegle-tweaks.user.js
+++ omegle-tweaks.user.js
@@ -1,22 +1,20 @@
-/*
-==UserScript==
-@name         Omegle-Tweaks
-@version      1.0
-@author       PWall
-@include      https://omegle.com/*
-@include      https://www.omegle.com/*
-@run-at       document-start
-@grant        none
-==/UserScript== 
-*/
+//==UserScript==
+//@name         Omegle-Tweaks
+//@version      1.0
+//@author       PWall
+//@include      https://omegle.com/*
+//@include      https://www.omegle.com/*
+//@run-at       document-start
+//@grant        none
+//==/UserScript==
 
 (async () => {
 	document.documentElement.innerHTML = "<head><title>Omegle</title></head><body></body>";
-	window.stop()
+	window.stop();
 	const doc = await fetch("").then((response) => response.text());
 	const item = document.createElement("iframe");
 	item.srcdoc = doc;
-	item.style = "position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none;"
+	item.style = "position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none;";
 	document.body.appendChild(item);
 	item.focus();
 })();
