3954eb4b390c181bd4f303cf2f3e196ce2040e85
PWall <34860495+PWall2222@users.noreply.github.com>
2021-06-08 20:31:36 +0200

    Commands: Added types to argument functions

--- src/ts/commands/functions/argument.ts
+++ src/ts/commands/functions/argument.ts
@@ -1,21 +1,22 @@
+import { UserCommand } from "commands/handler.js";
 import { sendMessage } from "extra/frontFunctions.js";
 import { session } from "index.js";
 import { settings } from "storage/settings.js";
 import { addCommand } from "ui/chat/add.js";
 import { setVolume } from "ui/nodes/video.js";
 
-const likes = function () {
+const likes = function (this: UserCommand) {
 	settings.likes = this.list;
 };
 
-const social = function () {
+const social = function (this: UserCommand) {
 	const arg = this.arguments[0];
 	if (arg && settings.socials[arg] !== undefined) {
 		sendMessage(`${arg}: ${settings.socials[arg]}`);
 	}
 };
 
-const set = function () {
+const set = function (this: UserCommand) {
 	const parsedArgs = JSON.parse(this.arguments[1]);
 	const arg = this.arguments[0];
 	if (typeof settings[arg] === typeof parsedArgs) {
@@ -25,13 +26,13 @@ const set = function () {
 	}
 };
 
-const send = function () {
+const send = function (this: UserCommand) {
 	if (session.connected) {
 		sendMessage(this.arguments.join(" "));
 	}
 };
 
-const volume = function () {
+const volume = function (this: UserCommand) {
 	const num = Number(this.arguments[0]);
 	if (num >= 0 && num <= 100) {
 		setVolume(num);
--- src/ts/commands/handler.ts
+++ src/ts/commands/handler.ts
@@ -35,4 +35,4 @@ class UserCommand {
 	}
 }
 
-export { commandHandler };
+export { commandHandler, UserCommand };
