6cdaf1bf8e4e5ac6f10e97742a21a911a605865c
PWall <34860495+PWall2222@users.noreply.github.com>
2023-01-10 14:27:06 +0100

    Resolve relative imports for ts

--- gulp/typescript.js
+++ gulp/typescript.js
@@ -1,24 +1,22 @@
 const { src, dest } = require("gulp");
 const changed = require("gulp-changed");
-const replace = require("gulp-replace");
+const alias = require('@gulp-plugin/alias');
 const sourceMaps = require("gulp-sourcemaps");
 const ts = require("gulp-typescript");
 const args = require("yargs").argv;
 
 const tsProject = ts.createProject("tsconfig.json");
 
-const url = args.url ?? "/";
 const output = args.output ?? "server";
-const pathRegEx = /(?<=(import|export)(?:.* from |\(| )["'])[^.].*(?=["']\)?)/g;
 
 const compileTS = () => {
 	return new Promise((resolve) => {
 		src("src/ts/**")
+			.pipe(alias(tsProject))
 			.pipe(sourceMaps.init())
 			.pipe(tsProject())
 			.on("error", () => {})
 			.pipe(sourceMaps.write("", { sourceRoot: "" }))
-			.pipe(replace(pathRegEx, `${url}$&`))
 			.pipe(changed(output, { hasChanged: changed.compareContents }))
 			.pipe(dest(output))
 			.on("end", resolve);
--- omclient.user.js
+++ omclient.user.js
@@ -12,8 +12,9 @@
 	document.documentElement.innerHTML = "<head><title>Omegle</title></head><body></body>";
 	window.stop();
 	const doc = await fetch("{magicurl}index.html").then((response) => response.text());
+	const modifiedDoc = doc.replace('href="/"', 'href="{magicurl}"')
 	const item = document.createElement("iframe");
-	item.srcdoc = doc;
+	item.srcdoc = modifiedDoc;
 	item.style = "position:fixed; inset:0; width:100%; height:100%; border:none;";
 	document.body.appendChild(item);
 	item.focus();
