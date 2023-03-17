a5f7e0a2aed3128d0c8cc7c75e39cd1846dd6692
PWall <34860495+PWall2222@users.noreply.github.com>
2023-03-17 14:38:44 +0100

    Relative imports for index.ts

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,16 +1,16 @@
-import { loadAll } from "@/extra/loader.js";
-import { media, setMedia } from "@/extra/media.js";
-import { connectionArgs } from "@/network/arguments.js";
-import { Backend } from "@/network/backend.js";
-import { eventHandler } from "@/network/events.js";
-import { createPC } from "@/network/webrtc.js";
-import { Session } from "@/storage/session.js";
-import { settings } from "@/storage/settings.js";
-import { clearAdd } from "@/ui/chat/add.js";
-import { autoClearChat } from "@/ui/chat/manager.js";
-import { errorHandler } from "@/ui/errorHandler.js";
-import { setDC } from "@/ui/nodes/disconnect.js";
-import { addMedia, addSpinner } from "@/ui/nodes/video.js";
+import { loadAll } from "./extra/loader.js";
+import { media, setMedia } from "./extra/media.js";
+import { connectionArgs } from "./network/arguments.js";
+import { Backend } from "./network/backend.js";
+import { eventHandler } from "./network/events.js";
+import { createPC } from "./network/webrtc.js";
+import { Session } from "./storage/session.js";
+import { settings } from "./storage/settings.js";
+import { clearAdd } from "./ui/chat/add.js";
+import { autoClearChat } from "./ui/chat/manager.js";
+import { errorHandler } from "./ui/errorHandler.js";
+import { setDC } from "./ui/nodes/disconnect.js";
+import { addMedia, addSpinner } from "./ui/nodes/video.js";
 
 const newChat = async () => {
 	session = new Session();
